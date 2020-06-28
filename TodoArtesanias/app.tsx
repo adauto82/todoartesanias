a08f4e38c1398eb04b88f13e97fdadb2206be066
Joaquin Diaz Trepat <joaquin.dt@gmail.com>
2020-06-28 19:15:34 -0300

    Changes for app.txs for runout

--- TodoArtesanias/app.tsx
+++ TodoArtesanias/app.tsx
@@ -2,7 +2,6 @@ declare var require: any
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-import CSS from 'csstype';
 
 
 const h1Styles = {
@@ -19,14 +18,37 @@ const h1Styles = {
 export class Header extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark"><h1>TodoArtesanias</h1></nav>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div className="container">
+                    <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar12"> 
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbar12">
+                        <a className="navbar-brand d-none d-md-block" href="#">
+                            <i className="fa d-inline fa-lg fa-circle"></i>
+                            <b>Tienda Online de Artesanos</b>
+                        </a>
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item"> <a className="nav-link" href="Home.html">Home</a> </li>
+                            <li className="nav-item"> <a className="nav-link" href="#">Tienda</a> </li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item"> <a className="nav-link" href="Login.html">Ingresar</a> </li>
+                            <li className="nav-item text-light"> <a className="nav-link text-primary" href="Registro.html">Registrarse</a> </li>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2 ml-3" type="text" placeholder="Busca diseños y artistas"/> <button className="btn my-2 my-sm-0 btn-outline-light" type="submit">Buscar</button> </form>
+                    </div>
+                </div>
+                <h1><b>Tienda Online de Artesanos</b></h1>
+            </nav>
         );
     }
 }
 
 export class Hello extends React.Component {
     render() {
-        return (
+        return ( 
             <div className="todoartesanias">
                 <Header />
                 <h1>Welcome to Tijuana!!</h1>
